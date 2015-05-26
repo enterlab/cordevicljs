cf94dd0f77dd0bd59f99ac53aa2df03e0a8fbbc0
Henrik Mohr <henrik@meewee.com>
2015-05-26 21:25:27 +0200

    Adding css/js/icons for angular and later use

--- resources/www/js/out/reagent/debug.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-3291 {}
-goog.provide('reagent.debug');
-goog.require('cljs.core');
-
-//# sourceMappingURL=debug.js.map
--- resources/www/js/out/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-3291 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map
