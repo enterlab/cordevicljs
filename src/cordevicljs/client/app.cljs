a94bb1d3820fba7f9c1ac9d7965b644ea9178024
Henrik Mohr <henrik@meewee.com>
2015-05-26 21:50:53 +0200

    Added OnsenUI

--- src/cordevicljs/client/app.cljs
+++ src/cordevicljs/client/app.cljs
@@ -32,7 +32,7 @@
   (.addEventListener js/document "deviceready" onDeviceReady true))
 
 (defn ^:export main []
-  (.module (.-angular js/window) "app" #js []) ; "onsen"
+  (.module (.-angular js/window) "app" #js ["onsen"])
   (prepare-device)
   (when-let [root (.getElementById js/document "app")]
     (reagent/render-component [app state] root))
