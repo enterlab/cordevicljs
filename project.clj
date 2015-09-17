457beb6e9972e0fce3e6f23ca5a036c8a2092fac
Henrik Mohr <henrik@meewee.com>
2015-09-17 09:26:04 +0200

    Updating cljs -> 1.7.122, environ -> 1.0.1, transit-cljs -> 0.8.225, reagent -> 0.5.1

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://enterlab.com"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.58"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.stuartsierra/component "0.2.3"]
-                 [environ "1.0.0"]
+                 [environ "1.0.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
 
@@ -17,9 +17,9 @@
 
                  [com.taoensso/sente "1.6.0" :exclusions [org.clojure/tools.reader]]
                  [com.cognitect/transit-clj "0.8.281" :exclusions [commons-codec]]
-                 [com.cognitect/transit-cljs "0.8.220"]
+                 [com.cognitect/transit-cljs "0.8.225"]
 
-                 [reagent "0.5.0"]]
+                 [reagent "0.5.1"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]]
 
